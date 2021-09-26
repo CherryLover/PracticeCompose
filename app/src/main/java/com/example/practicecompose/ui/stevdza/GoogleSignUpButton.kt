4758ff908e7de80e84b6a0f57dd85517fb463952
jiangjiwei <jiangjiwei0322@outlook.com>
2021-09-26 21:18:43 +0800

    feat: 🎸 add youtube channel stevdza_san practice

---
+++ app/src/main/java/com/example/practicecompose/ui/stevdza/GoogleSignUpButton.kt
@@ -0,0 +1,19 @@
+package com.example.practicecompose.ui.stevdza
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+/**
+ * @description
+ * @author: Created jiangjiwei in 2021/9/26 9:17 下午
+ */
+@Composable
+fun GoogleSignUpButton() {
+
+}
+
+@Preview
+@Composable
+fun GoogleSignUpButtonPreview() {
+
+}
