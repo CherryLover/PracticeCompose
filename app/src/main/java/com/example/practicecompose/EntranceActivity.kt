bb4ab43f9f12dc9fed1d4e88d587e8f8dd2260fd
jiangjiwei <jiangjiwei0322@outlook.com>
2021-09-27 13:08:04 +0800

    refactor: refactor page

---
+++ app/src/main/java/com/example/practicecompose/EntranceActivity.kt
@@ -0,0 +1,36 @@
+package com.example.practicecompose
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.example.practicecompose.page.GoogleSignUpActivity
+
+class EntranceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                SampleItem("GoogleSignUpButton") { GoogleSignUpActivity.start(this@EntranceActivity) }
+            }
+        }
+    }
+}
+
+@Composable
+fun SampleItem(text: String = "", onClicked: () -> Unit) {
+    Button(onClick = onClicked) {
+        Text(text = text)
+    }
+}
--- app/src/main/java/com/example/practicecompose/page/GoogleSignUpActivity.kt
+++ app/src/main/java/com/example/practicecompose/page/GoogleSignUpActivity.kt
@@ -1,5 +1,7 @@
 package com.example.practicecompose.page
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
@@ -11,6 +13,15 @@ import androidx.compose.ui.Modifier
 import com.example.practicecompose.ui.stevdza.GoogleSignUpButton
 
 class GoogleSignUpActivity : AppCompatActivity() {
+
+    companion object {
+        @JvmStatic
+        fun start(context: Context) {
+            val starter = Intent(context, GoogleSignUpActivity::class.java)
+            context.startActivity(starter)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
